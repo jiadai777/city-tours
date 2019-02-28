ef92f9b95fd11b6cc93b4cb082949e9637c73953
Jiadai Liu <jliuapp@gmail.com>
2019-02-28 09:52:24 -0800

    Add files via upload

---
+++ precache-manifest.47dc731569f4c9cacbcb33542777aa92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cebc2253744ee9c4d6fd",
+    "url": "/static/css/main.953c16ca.chunk.css"
+  },
+  {
+    "revision": "cebc2253744ee9c4d6fd",
+    "url": "/static/js/main.cebc2253.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d721eec376ea58457f9afbc4254c448",
+    "url": "/index.html"
+  }
+];
